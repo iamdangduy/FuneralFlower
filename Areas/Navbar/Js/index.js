d7b9ef7a5da65ac135c31bd374011bdf279ea7e1
Nguyễn Đăng Duy <nguyendangduy18082001@gmail.com>
2025-04-24 00:17:54 +0700

    change navbar

--- Areas/Navbar/Js/index.js
+++ Areas/Navbar/Js/index.js
@@ -1,12 +1,8 @@
-// Lấy URL hiện tại
-const currentPath = window.location.pathname;
-console.log(currentPath);
-
-// Thêm class active dựa trên đường dẫn
-if (currentPath === '/index.html') {
-    document.getElementById('home').classList.add('active');
-} else if (currentPath.includes('Areas/Products/Views/index.html')) {
-    document.getElementById('flowers').classList.add('active');
-} else if (currentPath.includes('tel:0876526226')) {
-    document.getElementById('contact').classList.add('active');
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
 }
